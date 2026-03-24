c48cd751c0fdf5e140ea101a58963dff27716927
kemra102 <1282143+kemra102@users.noreply.github.com>
2026-03-24 19:48:59 +0000

    tourmaline: update atuin to 18.13.5

--- build_files/tourmaline.sh
+++ build_files/tourmaline.sh
@@ -36,7 +36,7 @@ ln -s /usr/lib/systemd/system/fwupd-refresh.timer \
 
 
 # Install Atuin
-ATUIN_VERSION='18.13.3'
+ATUIN_VERSION='18.13.5'
 ATUIN_FILE_NAME="atuin-x86_64-unknown-linux-gnu" # without file extension
 TMP_DIR=$(mktemp -d)
 
