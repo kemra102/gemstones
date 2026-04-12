3d93dabaf9d251b00091fb6d9324a1afff59e08d
kemra102 <1282143+kemra102@users.noreply.github.com>
2026-04-12 23:23:35 +0100

    tourmaline: remove native firefox

--- build_files/tourmaline.sh
+++ build_files/tourmaline.sh
@@ -3,6 +3,10 @@
 set -ouex pipefail
 
 
+# We're gonna get Firefox from Flathub so remove the native version.
+dnf5 --assumeyes remove firefox firefox-langpacks
+
+
 # Disable Discover notifier as we automate updates in the background
 if [[ -f /etc/xdg/autostart/org.kde.discover.notifier.desktop ]]; then
     rm -f /etc/xdg/autostart/org.kde.discover.notifier.desktop
